39d70132260c402668f3430775295d4e9bd9bcb8
Chai T. Rex <ChaiTRex@users.noreply.github.com>
2021-10-25 11:33:53 -0400

    Site generator: better variable names, better output code

--- site-generator/src/main.rs
+++ site-generator/src/main.rs
@@ -2,26 +2,25 @@ use std::path::{Path, PathBuf};
 use walkdir::WalkDir;
 
 fn main() {
-    let src_path: PathBuf = std::iter::once("src").collect();
-    let dist_path: PathBuf = std::iter::once("dist").collect();
+    let src: PathBuf = std::iter::once("src").collect();
+    let dist: PathBuf = std::iter::once("dist").collect();
 
-    WalkDir::new("src")
+    WalkDir::new(&src)
         .follow_links(true)
         .sort_by_file_name()
         .into_iter()
         .filter_map(|entry| entry.ok())
         .map(|entry| entry.into_path())
         .filter(|path| path.is_file())
-        .map(|src| {
-            let dist = dist_path.join(src.strip_prefix(&src_path).unwrap());
-            (src, dist)
+        .map(|path_in_src| {
+            let path_in_dist = dist.join(path_in_src.strip_prefix(&src).unwrap());
+            (path_in_src, path_in_dist)
         })
-        .for_each(|(src, dist)| {
-            let bytes_copied = copy_and_create_dirs(&src, &dist).unwrap();
+        .for_each(|(path_in_src, path_in_dist)| {
+            print!("{:?} -> {:?}", path_in_src, path_in_dist);
+            let bytes_copied = copy_and_create_dirs(&path_in_src, &path_in_dist).unwrap();
             println!(
-                "{:?} -> {:?} ({} byte{})",
-                src,
-                dist,
+                " ({} byte{})",
                 bytes_copied,
                 if bytes_copied == 1 { "" } else { "s" }
             );
